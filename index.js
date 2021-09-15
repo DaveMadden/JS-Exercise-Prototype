36b02bd2374fb1371ccdc34e0dfddc7691ac13f8
Dave Madden <davelearnsstuff@gmail.com>
2021-09-15 12:39:11 -0700

    Person working except prototype inherit test

--- index.js
+++ index.js
@@ -39,12 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = [];
+  // this.eat = function (someFood){
+  //   if (this.stomach.length < 10){
+  //     this.stomach.push(someFood);
+  //   }
+  //   return this.stomach;
+  // }
+  // this.poop = function(){
+  //   this.stomach = [];
+  //   return this.stomach;
+  // }
+  // this.toString = function(){
+  //   return `${this.name}, ${this.age}`
+  // }
 }
+const dave = new Person('David', 37);
 
-
-
+console.log(dave.toString());
 
 
 
